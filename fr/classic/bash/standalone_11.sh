19e3b9573797f1f237b4c2fdc0ed2ef15b16bd91
justumen <me@justumen.org>
2018-07-02 20:19:55 +0200

    bash 11

--- fr/classic/bash/standalone_11.sh
+++ fr/classic/bash/standalone_11.sh
@@ -454,7 +454,7 @@ function start_quiz(){
   echo -e "- La réponse doit être la plus courte possible, une commande valide mais ajoutant des caractères inutiles ne fonctionnera pas."
   echo -e "Exemple : si la réponse est 'ls'. Les réponses 'ls .', 'ls ./' et 'ls ././' seront considérées comme fausses."
   answer_text_fr "Par quel mot clef doit se terminer une condition 'if' ?" "fi"
-  answer_text_fr "Quel argument pouvez-vous donner à 'if' qui garantira l'execution de son 'else' ?" "false"
+  answer_text_fr "Quel argument pouvez-vous donner à 'if' qui garantira l'exécution du contenu dans son 'else' correspondant ?" "false"
   answer_text_fr "Comment demander la valeur de la variable 'nom' à l'utilisateur ?" "read nom"
   answer_text_fr "En utilisant 'if', un seul '[' et un seul ']', comment afficher le mot 'oui' si la valeur de X est plus grande que 2 ?" 'if [ $X -gt 2 ];then echo oui;fi'
   answer_text_fr "Sans utiliser de 'if' et en utilisant qu'un seul '[' et qu'un seul ']', comment afficher le mot 'oui' si la valeur de X est plus petite que 2 ?" '[ $X -lt 2 ]&&echo oui'
