9f550597355ae9d3334b614fa83cf55293d1be53
justumen <me@justumen.org>
2018-07-02 20:10:04 +0200

    bash 11

--- fr/classic/bash/standalone_11.sh
+++ fr/classic/bash/standalone_11.sh
@@ -456,9 +456,9 @@ function start_quiz(){
   answer_text_fr "Par quel mot clef doit se terminer une condition 'if' ?" "fi"
   answer_text_fr "Quel argument pouvez-vous donner à 'if' qui garantira l'execution de son 'else' ?" "false"
   answer_text_fr "Comment demander la valeur de la variable 'nom' à l'utilisateur ?" "read nom"
-  answer_text_fr "En utilisant 'if', un seul '[' et un seul ']', comment afficher le mot 'oui' si la valeur de X est plus grande que 2 ?" "if [ $X -gt 2 ];then echo oui;fi"
-  answer_text_fr "Sans utiliser de 'if' et en utilisant qu'un seul '[' et qu'un seul ']', comment afficher le mot 'oui' si la valeur de X est plus petite que 2 ?" "[ $X -lt 2 ]&&echo oui"
-  answer_text_fr "Sans utiliser de 'if' ou de '[', comment afficher le mot 'oui' si la valeur de X est égal à 2 ?" "test $X -eq 2&&echo oui"
+  answer_text_fr "En utilisant 'if', un seul '[' et un seul ']', comment afficher le mot 'oui' si la valeur de X est plus grande que 2 ?" 'if [ $X -gt 2 ];then echo oui;fi'
+  answer_text_fr "Sans utiliser de 'if' et en utilisant qu'un seul '[' et qu'un seul ']', comment afficher le mot 'oui' si la valeur de X est plus petite que 2 ?" '[ $X -lt 2 ]&&echo oui'
+  answer_text_fr "Sans utiliser de 'if' ou de '[', comment afficher le mot 'oui' si la valeur de X est égal à 2 ?" 'test $X -eq 2&&echo oui'
   unlock "bash" "11" "2211" "ddfb"
 }
 
