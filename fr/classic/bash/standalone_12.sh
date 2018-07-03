8d9ac2a66003cdafaf59d4876b218680854b7614
justumen <me@justumen.org>
2018-07-03 20:48:17 +0200

    bash video

--- fr/classic/bash/standalone_12.sh
+++ fr/classic/bash/standalone_12.sh
@@ -98,13 +98,13 @@ function new_video(){
 	#~ $SOUNDPLAYER "$AUDIO_LOCAL/$restore.mp3" &> /dev/null &
 	#~ mpv "$VIDEO_LOCAL/$restore.mp3.mp4" &> /dev/null &
 	ADD_PLAYLIST "$VIDEO_LOCAL/$restore.mp3.mp4"
-	#~ sleep 1
+	sleep 1
 	PLAYLIST_NEXT #play next video
-	#~ sleep 1
+	sleep 1
 	LOOP_OFF
-	#~ sleep 1
+	sleep 1
 	UNTIL_IDLE_IS_BACK
-	#~ sleep 1
+	sleep 1
 	#~ PLAYLIST_NEXT #play idle video
 	#~ sleep 1
 	LOOP_ON #After normal video is over, quickly put back LOOP
