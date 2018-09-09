12ae21955985a1ccb8a26aabaee3842f3b04b6bf
justumen <me@justumen.org>
2018-09-09 18:11:25 +0700

    ...

--- fr/classic/bash/standalone_9.sh
+++ fr/classic/bash/standalone_9.sh
@@ -82,7 +82,7 @@ function press_key(){
 
 #TODO ???
 function new_sound(){
-	VOICE_PID=$(ps -|grep "$SOUNDPLAYER"|grep -v grep|grep -v MUSIC|awk '{print $2}')
+	VOICE_PID=$(ps -f|grep "$SOUNDPLAYER"|grep -v grep|grep -v MUSIC|awk '{print $2}')
 	#~ echo "VOICE_PID = $VOICE_PID"
 	if [[ "$VOICE_PID" != "" ]]; then
 		kill $VOICE_PID
