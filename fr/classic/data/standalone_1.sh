35b6821bc2157e9bd6686b0abbf210410ab2eaec
justumen <me@justumen.org>
2018-10-13 12:15:38 +0700

    ../..

---
+++ fr/classic/data/standalone_1.sh
@@ -0,0 +1,517 @@
+#!/bin/bash
+#SOME ADDED AND CHANGE IN CLI learn_cli.sh in CLASSIC
+shopt -s expand_aliases
+source ~/.bashrc
+
+function pause_music(){
+	kill -SIGTSTP $1
+}
+function start_quiz_music(){
+	if [[ "$MUTE" == "0" ]]; then
+		MUSIC_PID=$(ps -ef|grep "$SOUNDPLAYER_MUSIC"|grep Music|grep -v quiz|awk '{print $2}'|head -n 1)
+		if [[ "$MUSIC_PID" != "" ]]; then
+			pause_music $MUSIC_PID
+		fi
+		$SOUNDPLAYER_MUSIC_QUIZ /home/umen/.GameScript/Sounds/default/Music/quiz_1.mp3 &>/dev/null &
+	fi
+}
+function stop_quiz_music(){
+	if [[ "$MUTE" == "0" ]]; then
+		MUSIC_QUIZ_PID=$(ps -ef|grep "$SOUNDPLAYER_MUSIC_QUIZ"|grep quiz|awk '{print $2}'|head -n 1)
+		if [[ "$MUSIC_QUIZ_PID" != "" ]]; then
+			kill $MUSIC_QUIZ_PID
+		fi
+	fi
+}
+function download_all_sounds(){
+	cd $AUDIO_LOCAL || exit
+	i=4
+	rm to_dl.wget 2> /dev/null
+	echo "Downloading Audio..."
+	while [ $i -le $LINES ]; do
+		echo "$AUDIO_DL/$i.mp3" >> to_dl.wget
+		i=`expr $i + 1`
+	done
+	cat to_dl.wget | xargs -n 1 -P 4 wget -q &
+}
+function download_all_videos(){
+	cd $VIDEO_LOCAL || exit
+	i=4
+	rm to_dl.wget 2> /dev/null
+	echo "Downloading Videos..."
+	while [ $i -le $LINES ]; do
+		echo "$VIDEO_DL/$i.mp3.mp4" >> to_dl.wget
+		i=`expr $i + 1`
+	done
+	cat to_dl.wget | xargs -n 1 -P 4 wget -q &
+}
+function prepare_audio(){
+	AUDIO_LOCAL="$HOME/.GameScript/Audio/$LANGUAGE/classic/$CHAPTER_NAME/$SPEAKER/c$CHAPTER_NUMBER"
+	mkdir -p $AUDIO_LOCAL 2> /dev/null
+	AUDIO_DL="https://raw.githubusercontent.com/justUmen/GameScript/master/$LANGUAGE/classic/$CHAPTER_NAME/Audio/$SPEAKER/c$CHAPTER_NUMBER"
+	AUDIOCMP=1
+	if [[ "$MUTE" != 1 ]] && [[ "$VOICE" != "0" ]];then
+		if [ ! -f "$AUDIO_LOCAL/4.mp3" ]; then
+			wget -q --spider http://google.com
+			if [ $? -eq 0 ]; then
+				download_all_sounds
+			else
+				echo "Cannot download audio, no internet ?"
+			fi
+		fi
+	fi
+}
+function prepare_video(){
+	VIDEO_LOCAL="$HOME/.GameScript/Video/$LANGUAGE/classic/$CHAPTER_NAME/$SPEAKER/c$CHAPTER_NUMBER"
+	mkdir -p $VIDEO_LOCAL 2> /dev/null
+	VIDEO_DL="https://raw.githubusercontent.com/justUmen/GameScript/master/$LANGUAGE/classic/$CHAPTER_NAME/Video/$SPEAKER/c$CHAPTER_NUMBER"
+	VIDEOCMP=1
+	if [ ! -f "$VIDEO_LOCAL/4.mp3.mp4" ]; then
+		wget -q --spider http://google.com
+		if [ $? -eq 0 ]; then
+			download_all_videos
+		else
+			echo "Cannot download videos, no internet ?"
+		fi
+	fi
+}
+function encode_b64(){
+	echo -n "$2$1$3" | base64
+}
+function press_key(){
+	echo -en "\e[0;33m...\e[0m"
+	read -s -n1 key < /dev/tty
+	if [ "$key" == 'e' ]; then
+		exit
+	fi
+}
+
+#TODO ???
+function new_sound(){
+	VOICE_PID=$(ps -f|grep "$SOUNDPLAYER"|grep -v grep|grep -v MUSIC|awk '{print $2}'|head -n 1)
+	if [[ "$VOICE_PID" != "" ]]; then
+		kill $VOICE_PID
+	fi
+	$SOUNDPLAYER "$AUDIO_LOCAL/$restore.mp3" &> /dev/null &
+}
+
+function new_video(){
+	VIDEO_PID=$(ps -ef|grep "mpv --really-quiet --input-ipc-server=/tmp/southpark"|grep -v grep|awk '{print $2}'|head -n 1)
+	if [[ "$VIDEO_PID" == "" ]]; then
+		rm /tmp/southpark
+		mpv --really-quiet --input-ipc-server=/tmp/southpark --no-config --include=~/.GameScript/mpv_config --loop=no $VIDEO_LOCAL/$restore.mp3.mp4 &
+		sleep 2
+		VIDEO_PID=$(ps -ef|grep "mpv --really-quiet --input-ipc-server=/tmp/southpark"|grep -v grep|awk '{print $2}'|head -n 1)
+		while [[ "$VIDEO_PID" == "" ]]; do
+			VIDEO_PID=$(ps -ef|grep "mpv --really-quiet --input-ipc-server=/tmp/southpark"|grep -v grep|awk '{print $2}'|head -n 1)
+			sleep 1
+		done
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+	else
+		echo "{ \"command\": [\"loadfile\", \"$VIDEO_LOCAL/$restore.mp3.mp4\", \"replace\"] }" | socat - /tmp/southpark &> /dev/null
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+		echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+	fi
+}
+
+function talk(){
+	if [[ $VIDEO == 0 ]]; then 
+		if [[ $MUTE == 0 ]]; then 
+			new_sound
+		fi
+	else
+		new_video
+	fi
+	echo -e "($restore)\e[0;32m $1\e[0m - $2"
+
+	#??? test delay avoid past read input
+	read -s -t 1 -n 10000 discard
+
+	#~ sleep 1
+	#~ while read -r -t 0; do read -r; done
+	
+	#~ sleep 0.5
+	#~ read -s -e -t 0.1 #flush stdin ?
+
+	press_key
+}
+function talk_not_press_key(){
+	if [[ $VIDEO == 0 ]]; then 
+		if [[ $MUTE == 0 ]]; then 
+			new_sound
+		fi
+	else
+		new_video
+	fi
+	echo -e "($restore)\e[0;32m $1\e[0m - $2"
+}
+function talk_not_press_key_ANSWER(){
+	echo -en "($restore)\e[0;32m $1\e[0m - "
+	echo -ne "\\e[4;37m"
+	echo -nE "$2"
+	echo -e "\\e[0m"
+}
+function how_to_leave_chapter(){
+	echo ""
+	case $LANGUAGE in
+		fr) echo "Pour fermer ce chapitre, appuyez sur la touche 'e' quand vous voyez ces ... " ;;
+		en) echo "To close this chapter, press the key 'e' when you see these '...'" ;;
+	esac
+	echo ""
+}
+
+function answer_quiz(){
+	#$7 = bash, from enter_chapter
+	# echo " ---> $7 <--- "
+	echo -e "      \\e[0;100m 1) \\e[0m $1"
+	echo -e "      \\e[0;100m 2) \\e[0m $2"
+	echo -e "      \\e[0;100m e) \\e[0m $3"
+	key="9"
+	while [ "$key" != "1" ] && [ "$key" != "2" ] && [ "$key" != "e" ]; do
+		echo -en "      \\e[97;45m # \\e[0m"	
+		read key < /dev/tty
+	done
+		# echo ""
+		#~ echo -e "\\e[0;100m 0) \\e[0m Télécharger audio en avance"
+		case $key in
+			0) 	if [[ $VIDEO == 0 ]]; then download_all_sounds; else download_all_videos; fi ;;
+			1) 	case $LANGUAGE in
+					fr) ANSWER_QUIZ_TEXT="$HOME/.GameScript/restore_$7$8 existe, continuer ou recommencer le cours du début ?"
+						TEXT_CONTINUE="Continuer"
+						TEXT_RESTART="Recommencer"
+						TEXT_BACK="Retour"
+					;;
+					en) ANSWER_QUIZ_TEXT="$HOME/.GameScript/restore_$7$8 already exists, continue or start lecture from the beginning ?"
+						TEXT_CONTINUE="Continue"
+						TEXT_RESTART="Restart"
+						TEXT_BACK="Back"
+					;;
+				esac
+				if [ -f "$HOME/.GameScript/restore_$7$8" ];then
+					echo "$ANSWER_QUIZ_TEXT"
+					echo -e "      \\e[0;100m 1) \\e[0m $TEXT_CONTINUE"
+					echo -e "      \\e[0;100m 2) \\e[0m $TEXT_RESTART"
+					echo -e "      \\e[0;100m e) \\e[0m $TEXT_BACK"
+					choice="x"
+					while [ "$choice" != "1" ] && [ "$choice" != "2" ] && [ "$choice" != "3" ]; do
+						echo -en "      \\e[97;45m # \\e[0m"
+						read choice < /dev/tty
+					done
+						case $choice in
+							1)  cd `cat "$HOME/.GameScript/restore_pwd_$CHAPTER_NAME$CHAPTER_NUMBER"`
+								how_to_leave_chapter
+								start_lecture `cat "$HOME/.GameScript/restore_$CHAPTER_NAME$CHAPTER_NUMBER"`
+								start_quiz
+								;;
+							2) 	clean
+								how_to_leave_chapter
+								start_lecture 1
+								start_quiz
+								;;
+							e) exit ;;
+						esac
+					#~ done
+				fi
+				start_lecture 1
+				start_quiz
+				;;
+			2) 	start_quiz ;;
+			e) exit ;;
+		esac
+	#~ done
+}
+
+QUIZ_NUMBER=1
+function answer_text_fr(){
+	echo ""
+	echo "($QUIZ_NUMBER) > $1"
+	#~ echo -en "\\e[97;45m # \\e[0m"
+	read -e -r -p $'\e[97;45m # \e[0m' USER_CODE < /dev/tty
+	if [ ! "$USER_CODE" == "$2" ]; then
+		case $LANGUAGE in
+			fr) talk_not_press_key justumen "\\e[4;37mDésolé, réponse fausse ou trop longue. Je vous conseille de suivre / refaire le cours.\nSi vous pensez maitriser le contenu du cours, il y a surement un piège, relisez donc attentivement la question. :-)\nSi vous vous sentez vraiment bloqué, demandez de l'aide sur notre chat : https://rocket.bjornulf.org ou notre discord : https://discord.gg/25eRgvD\\e[0m"
+				press_key
+				stop_quiz_music
+				#enter_chapter $CHAPTER_NAME $CHAPTER_NUMBER
+				exit ;;
+			en) talk_not_press_key justumen "\\e[4;37mSorry answer wrong or too long. I recommend you to do / re-do the lecture :-)\nIf you think you already understand the content of the lecture, this question is probably a trap, read the question again carefully. :-)\nIf you feel stuck, ask for help on our discord : https://discord.gg/Dj47Tpf\\e[0m"
+				#enter_chapter $CHAPTER_NAME $CHAPTER_NUMBER
+				press_key
+				stop_quiz_music
+				exit ;;
+		esac
+	else
+		talk_not_press_key justumen "Correct !"
+		QUIZ_NUMBER=`expr $QUIZ_NUMBER + 1`
+	fi
+}
+function answer_run(){
+	#~ echo -en "\\e[97;45m # \\e[0m"
+	read -e -r -p $'\e[97;45m # \e[0m' USER_CODE < /dev/tty
+	while [ ! "$USER_CODE" == "$1" ]; do
+		if [ ! "$USER_CODE" == "" ]; then
+			talk_not_press_key_ANSWER "$2" "$1"
+		fi
+		#~ echo -en "\\e[97;45m # \\e[0m"
+		read -e -r -p $'\e[97;45m # \e[0m' USER_CODE < /dev/tty
+	done
+	if [ ! "$1" == "" ];then
+		echo -e "\e[1m"
+		printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+		#Quick solve for bash10 pb "alias a1='ls -a /var'" ???
+		#~ case $1 in
+			#~ a1) eval ls -a /var ;;
+			#~ alias) alias ;;
+			#~ *) eval "$1" ;;
+		#~ esac
+		eval "$1"
+		printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+		echo -e "\\e[0m"
+	fi
+}
+function start_dots(){
+	echo -en "\e[0;33m...\e[0m"
+}
+
+function title(){
+	echo -e "\e[1m"
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+	echo "$1"
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+	echo -e "\\e[0m"
+}
+
+black_on_green='\e[30;48;5;82m'
+black_on_lightblue='\e[30;48;5;81m'
+black_on_red='\e[41m'
+On_Black='\e[97;40m'
+reset='\e[0m'
+
+basic=$On_Black
+code=$black_on_lightblue
+codeFile=$black_on_green
+codeError=$black_on_red
+
+#UNDERLINE
+#~ voc='\e[1m'
+voc='\e[4;37m'
+#BLUE
+learn='\e[40;38;5;10m'
+
+MUTE=0
+if [ "$1" == "MUTE" ]; then
+	MUTE=1
+fi
+VIDEO=0
+if [ "$1" == "VIDEO" ]; then
+	VIDEO=1
+fi
+#~ command -v mplayer &> /dev/null && SOUNDPLAYER="mplayer -af volume=10" || SOUNDPLAYER="mpg123 --scale 100000";
+#SET in gamescript.sh too (need to be similar)
+command -v mplayer &> /dev/null && SOUNDPLAYER="mplayer -volume 100" || SOUNDPLAYER="mpg123";
+command -v mplayer &> /dev/null && SOUNDPLAYER_MUSIC="mplayer -volume 35" || SOUNDPLAYER_MUSIC="mpg123 --scale 11445"
+command -v mplayer &> /dev/null && SOUNDPLAYER_MUSIC_QUIZ="mplayer -volume 50" || SOUNDPLAYER_MUSIC_QUIZ="mpg123"
+
+#OBSOLETE ?
+#~ restore=2 #first line of LIST_4GEN should be environment test (test ~/House)
+restore=1
+function new_game_or_restore(){
+	#restore, 1 new game, otherwise x in file PERSONAL_PROGRESS
+	restore=1
+	if [ -e "~/PERSONAL_PROGRESS" ]; then
+		restore=`cat ~/PERSONAL_PROGRESS`
+	else
+		echo -n "1" > ~/PERSONAL_PROGRESS
+	fi
+	if [ "$1" ];then
+		restore=`expr $1 - 1`
+		echo -n "$1" > ~/PERSONAL_PROGRESS
+	fi
+}
+
+#~ function normal_line(){
+	#~ restore=$1
+	#~ play_mp3 "$restore"
+	#~ print_line "$line"
+	#~ press_key
+#~ }
+
+function play_mp3(){
+	if [ -e "sounds/$1.mp3" ];then
+		mpg123 "sounds/$1.mp3" > /dev/null 2>&1 &
+	fi
+}
+function event(){
+	echo -e "\\e[0;100m $1\\e[0m"
+	press_key
+	#~ restore=$(expr $restore + 1)
+}
+
+
+
+
+
+
+
+function unlock(){
+	#Usage : unlock "bash" "1" "24d8" "f016"
+	PSEUDO=`cat "$HOME/.GameScript/username"`
+	PASS=`encode_b64 $PSEUDO "$3" "$4"`
+	echo ""
+	case $LANGUAGE in
+		fr) echo -e "Pour débloquer '$1 $2' sur rocketchat (https://rocket.bjornulf.org), ouvrez une conversation privée avec '@boti' et copiez/collez :\n\t\e[97;42mpassword$PASS\e[0m"
+			echo -e "Pour débloquer '$1 $2' sur discord (https://discord.gg/25eRgvD), ouvrez le channel '#mots-de-passe-boti' et copiez/collez :\n\t\e[97;42mpassword$PASS\e[0m"
+			command -v xclip &> /dev/null && echo "Ce mot de passe a été copié automatiquement avec 'xclip'." || echo "[ Installez 'xclip' pour copier ce mot de passe automatiquement après un questionnaire. ]"
+			;;
+		en) #echo -e "To unlock '$1 $2' on rocketchat (https://rocket.bjornulf.org), open a private conversation with '@boti' and copy/paste :\n\t\e[97;42mpassword$PASS\e[0m"
+			echo -e "To unlock '$1 $2' on discord (https://discord.gg/Dj47Tpf), open the channel '#passwords-boti' and copy/paste :\n\t\e[97;42mpassword$PASS\e[0m"
+			command -v xclip &> /dev/null && echo "This password was automaticaly copied with 'xclip'." || echo "[ Install 'xclip' to copy this password automaticaly after a quiz. ]"
+			;;
+	esac
+	command -v xclip &> /dev/null && echo "$PASS" | xclip -i -selection clipboard
+	#AUTOMATICALLY DO THIS ?
+	touch "$HOME/.GameScript/good_$1$2" 2> /dev/null
+	mkdir $HOME/.GameScript/passwords/ 2> /dev/null
+	echo -n "$PASS" > "$HOME/.GameScript/passwords/$1$2"
+	press_key
+	echo ""
+	exit
+}
+
+function enter_chapter(){
+	#Usage : enter_chapter bash 1 1 (first 1 is chapter, next one is for case)
+	echo ""
+	case $LANGUAGE in
+		fr) echo -e "      \e[97;44m - $1, Chapitre $2 \e[0m"
+			answer_quiz "Cours" "Questionnaire" "Retour" "4" "5" "6" "$1" "$2" ;;
+		en) echo -e "      \e[97;44m - $1, Chapter $2 \e[0m"
+			answer_quiz "Lecture" "Quiz" "Back" "4" "5" "6" "$1" "$2" ;;
+	esac
+}
+
+
+
+
+
+
+
+
+
+
+#VIDEO SOUTH PARK
+function UNTIL_IDLE_IS_BACK(){
+	OUT=$(echo '{ "command": ["get_property", "filename"] }' | socat - /tmp/southpark)
+	#~ echo $OUT
+	while [ "$OUT" != '{"data":"10FPS_idle.mp4","error":"success"}' ]; do
+		OUT=$(echo '{ "command": ["get_property", "filename"] }' | socat - /tmp/southpark)
+		sleep 1
+	done
+}
+function PAUSE(){
+	echo '{ "command": ["set_property", "pause", true] }' | socat - /tmp/southpark &> /dev/null
+}
+function UNPAUSE(){
+	echo '{ "command": ["set_property", "pause", false] }' | socat - /tmp/southpark &> /dev/null
+}
+function LOOP_ON(){
+	echo '{ "command": ["set_property", "loop", true] }' | socat - /tmp/southpark &> /dev/null
+}
+function LOOP_OFF(){
+	echo '{ "command": ["set_property", "loop", false] }' | socat - /tmp/southpark &> /dev/null
+}
+function PLAYLIST_NEXT(){
+	echo '{ "command": ["playlist-next"] }' | socat - /tmp/southpark &> /dev/null
+}
+function PLAYLIST_CLEAR(){
+	echo '{ "command": ["playlist-clear"] }' | socat - /tmp/southpark &> /dev/null
+}
+function ADD_PLAYLIST(){
+	echo "{ \"command\": [\"loadfile\", \"$1\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+	sleep 0.5
+	echo "{ \"command\": [\"loadfile\", \"$HOME/.GameScript/10FPS_idle.mp4\", \"append\"] }" | socat - /tmp/southpark &> /dev/null
+}
+function WAIT_FOR_USER(){
+	echo -n "LINE = "
+	read line
+}
+
+#WORKING TEST CODE
+#~ mpv --really-quiet --input-ipc-server=/tmp/southpark --playlist mpv_playlist.txt &
+#~ sleep 2
+#~ LOOP_ON #play idle
+#~ while true; do
+	#~ WAIT_FOR_USER
+	
+	#~ PLAYLIST_NEXT #play next video
+	#~ echo "PLAY VIDEO"
+	#~ LOOP_OFF
+	
+	#~ UNTIL_IDLE_IS_BACK
+	#~ echo "BACK"
+	
+	#~ echo "PLAY IDLE"
+	#~ LOOP_ON
+#~ done
+
+function start_lecture(){
+restore=$1
+case $1 in
+1) echo -n 1 > $HOME/.GameScript/restore_data1; echo -n $(pwd) > $HOME/.GameScript/restore_pwd_data1; talk justumen ""; restore=$(expr $restore + 1) ;&
+esac
+}
+function clean(){ #in enter_chapter
+rm $HOME/.GameScript/restore_$CHAPTER_NAME$CHAPTER_NUMBER 2> /dev/null
+rm $HOME/.GameScript/restore_pwd_$CHAPTER_NAME$CHAPTER_NUMBER 2> /dev/null						
+	rm $HOME/House/Room/virus0 2> /dev/null
+	rm $HOME/House/Room/virus1 2> /dev/null
+	rm $HOME/House/Room/virus2 2> /dev/null
+	rm $HOME/House/Room/virus3 2> /dev/null
+	rm $HOME/House/Room/virus4 2> /dev/null
+	rmdir $HOME/House/Room/bed 2> /dev/null
+	rmdir $HOME/House/Room/closet 2> /dev/null
+	rmdir $HOME/House/Room/desk 2> /dev/null
+	rmdir $HOME/House/Room/ 2> /dev/null
+	rmdir $HOME/House/ 2> /dev/null
+	rmdir $HOME/.GameScript_bash1 2> /dev/null
+}
+
+function start_quiz(){
+  start_quiz_music
+  echo ""
+  echo -e "\e[15;44m Bash 'Bourne Again SHell' : Quiz Chapter 1 \e[0m"
+  echo -e "- The answer should be as short as possible, a good answer adding uneeded characters will be considered as wrong."
+  echo -e "Example : If the answer is 'ls'. The answers 'ls .', 'ls ./' and 'ls ././' won't work." 
+  answer_text_en "Which symbol is used to represent the root directory on Linux ?" "/"
+  answer_text_fr "Which command should you use to display the current directory ?" "pwd"
+  answer_text_fr "Which command displays the content of the root directory ?" "ls /"
+  answer_text_fr "Which command changes the current directory of the terminal with its parent ?" "cd .."
+  answer_text_fr "Which command displays the content of the current directory ?" "ls"
+  answer_text_fr "Which command deletes the empty folder 'test' from the current directory ?" "rmdir test"
+  answer_text_fr "By which symbol does an absolute path start with ?" "/"
+  answer_text_fr "The relative path of a file is often shorter than its equivalent absolute path. (true/false)" "true"
+  answer_text_fr "Which command can delete the file /home/test whatever your current directory is ?" "rm /home/test"
+  unlock "bash" "1" "24d8" "f016"
+}
+
+
+CHAPTER_NAME="bash"
+CHAPTER_NUMBER="1"
+LANGUAGE="en"
+SPEAKER="m1"
+
+LINES=187
+#~ if [ "$1" == "VIDEO" ]; then
+	#~ prepare_video
+#~ else
+	#~ if [ ! "$1" == "MUTE" ]; then
+		#~ prepare_audio
+	#~ fi
+#~ fi
+
+enter_chapter $CHAPTER_NAME $CHAPTER_NUMBER
